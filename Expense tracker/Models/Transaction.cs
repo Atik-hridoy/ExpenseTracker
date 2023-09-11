77b62a822866caf5d680eb3bff14c58604179be2
Md. Atikuzzamn Riday <tikahmedridoy@gmail.com>
2023-09-11 16:39:55 +0600

    first commit

---
+++ Expense tracker/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        //CatID
+        public int CatagoryID { get; set; }
+        public Catagory Catagory { get; set; }
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "navchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
